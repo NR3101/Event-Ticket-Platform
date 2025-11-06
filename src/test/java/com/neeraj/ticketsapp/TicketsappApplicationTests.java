54a0290c2f3d4995814ac7d8a061387e0bca827d
NR3101 <neeraj2000rai@gmail.com>
2025-11-06 19:31:51 +0530

    added initial config for postgres,mapstruct and keycloak

---
+++ src/test/java/com/neeraj/ticketsapp/TicketsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neeraj.ticketsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
