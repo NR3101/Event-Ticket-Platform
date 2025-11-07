546025062b16c167826f07aa9913d33d84de2412
NR3101 <neeraj2000rai@gmail.com>
2025-11-07 12:22:30 +0530

    added configuration for JPA auditing and security, including UserProvisioningFilter and SecurityConfig

---
+++ src/main/java/com/neeraj/ticketsapp/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.neeraj.ticketsapp.repositories;
+
+import com.neeraj.ticketsapp.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
