9a38c63383059e2d6cd33b4d6458bc362e0e7e5c
NR3101 <neeraj2000rai@gmail.com>
2025-11-12 19:24:32 +0530

    feat: added JWT authentication converter and integrate it into security configuration to implement ROLE BASED auth for some endpoints

---
+++ src/main/java/com/neeraj/ticketsapp/config/JwtAuthenticationConverter.java
@@ -0,0 +1,37 @@
+package com.neeraj.ticketsapp.config;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class JwtAuthenticationConverter implements Converter<Jwt, JwtAuthenticationToken> {
+    @Override
+    public JwtAuthenticationToken convert(Jwt jwt) {
+        Collection<SimpleGrantedAuthority> authorities = extractAuthorities(jwt);
+        return new JwtAuthenticationToken(jwt, authorities);
+    }
+
+    private Collection<SimpleGrantedAuthority> extractAuthorities(Jwt jwt) {
+        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+
+        if (realmAccess == null || !realmAccess.containsKey("roles")) {
+            return Collections.emptyList();
+        }
+
+        @SuppressWarnings("unchecked")
+        List<String> roles = (List<String>) realmAccess.get("roles");
+
+        return roles.stream()
+                .filter(role -> role.startsWith("ROLE_"))
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+    }
+}
